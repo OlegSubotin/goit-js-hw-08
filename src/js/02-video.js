472b5f49757a48ee34af751f2a42a094d0fa8d74
Oleg Subotin <subotinoleg0@gmail.com>
2022-02-21 00:21:30 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player'
+import throttle from 'lodash.throttle';
 
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+    const KEY = "videoplayer-current-time";
+    
+    player.on('timeupdate', throttle(onPlay, 1000));
+    setCurrentTime();       
+    
+function onPlay(time) {
+    localStorage.setItem(KEY, time.seconds.toString());
+}
+
+function setCurrentTime(){
+    player.setCurrentTime(localStorage.getItem(KEY));
+}
