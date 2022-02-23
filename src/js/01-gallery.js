a4081aae4951e2198f68c0abf686cccdac1b2a12
Oleg Subotin <subotinoleg0@gmail.com>
2022-02-23 13:05:03 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,15 +5,11 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 import { galleryItems } from './gallery-items';
 
-
-
 const galleryRef = document.querySelector('.gallery');
 
 const galleryMarkup = createImagesMarkup(galleryItems);
 
 galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
-
-galleryRef.addEventListener('click', onImgClick);
 
 function createImagesMarkup(img) {
     return img.map(item =>
@@ -26,19 +22,10 @@ function createImagesMarkup(img) {
     ).join('');
 }
 
-function onImgClick(event) {
-    event.preventDefault();
-
-    const isImageEl = event.target.classList.contains('gallery__image');
-    if (!isImageEl) {
-        return;
-    }
-
     let gallery = new SimpleLightbox('.gallery a');
     gallery.on('show.simplelightbox', function () {
         captionsData: "alt";
         captionDelay: "250";
 });
-}
 
 console.log(galleryItems);
