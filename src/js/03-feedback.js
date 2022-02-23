2fed852a282654261409e59b748cd64e6ca616fe
Oleg Subotin <subotinoleg0@gmail.com>
2022-02-24 01:54:37 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,47 +2,45 @@ import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = "feedback-form-state";
 
-let formData = {};
+const formEl = document.querySelector('.feedback-form');
 
-let formEl = document.querySelector('.feedback-form');
-const emailEl = document.querySelector(".feedback-form input");
-const textareaEl = document.querySelector('.feedback-form textarea');
+initForm();
 
 formEl.addEventListener('submit', onFormSubmit);
-formEl.addEventListener('input', throttle(onTextareaInput, 500));
 
-populateTextarea()
+formEl.addEventListener('input', throttle(onFormInput, 500));
 
 function onFormSubmit(evt) {
     evt.preventDefault();
+
     const { elements: { email, message } } = evt.target;
-    if (email.value === '' || message.value === '') { 
-    return
-    }
-    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
-    console.log('Форму отправил');
-    evt.target.reset();
-    formData = {};
-    localStorage.removeItem(STORAGE_KEY);
-}
 
-function onTextareaInput(evt) {
-    formData[evt.target.name] = evt.target.value;
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-}
-
-function populateTextarea() {
-    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-    if (!savedData) {
-    return
+    if (email.value === '' || message.value === ''){
+        return alert('все поля должны быть заполнены');
+    } else {
+        console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+        console.log("Отправил форму");
+        evt.target.reset();
+        localStorage.removeItem(STORAGE_KEY);
     }
     
-    if (savedData.email) {
-    formEl.email.value = savedData.email;
-    }
-    
-    if (savedData.message) {
-    formEl.message.value = savedData.message;
+}
+
+function onFormInput(evt) {
+    let persistedData = localStorage.getItem(STORAGE_KEY);
+    persistedData = persistedData ? JSON.parse(persistedData) : {};
+    persistedData[evt.target.name] = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(persistedData));
+}
+
+function initForm() {
+    let savedData = localStorage.getItem(STORAGE_KEY);
+
+    if (savedData) {
+        savedData = JSON.parse(savedData);
+        console.log(savedData);
+        Object.entries(savedData).forEach(([name, value]) => {
+        formEl.elements[name].value=value;
+        });
     }
 }
